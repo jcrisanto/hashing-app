39be26960398bea6174058cdf763a1016d917f68
Jorge Crisanto <jcrisanto0@gmail.com>
2022-09-19 16:18:22 -0600

    gen: trying again syntax 7

--- terraform/aws/modules/ec2/main.tf
+++ terraform/aws/modules/ec2/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "hashing_app_server" {
   ami           = "ami-068663a3c619dd892"
   instance_type = "t2.micro"
+  key_name = "hashing_app_server_key"
 
   tags = {
     Name = var.instance_name,
